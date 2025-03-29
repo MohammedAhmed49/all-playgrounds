66a1dfb872958348f7feb8d9553e20a4571aca32
Mohammed Ahmed <mohammed_ahmed4949@yahoo.com>
2025-03-29 06:43:58 +0200

    investment-app/feature: investment results table and logic

--- investment-calculations-app/src/app/calculator/calculator.component.ts
+++ investment-calculations-app/src/app/calculator/calculator.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { UserInputComponent } from './user-input/user-input.component';
 import { AnnualData, InvestmentData } from './calculator.model';
 import { calculateInvestmentResults } from '../../investment-results';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [UserInputComponent],
+  imports: [UserInputComponent, InvestmentResultsComponent],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.css',
 })
@@ -14,6 +15,5 @@ export class CalculatorComponent {
   calculatorData: AnnualData[] = [];
   onCalculate = (investmentData: InvestmentData) => {
     this.calculatorData = calculateInvestmentResults(investmentData);
-    console.log(this.calculatorData);
   };
 }
