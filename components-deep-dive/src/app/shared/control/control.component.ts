1b179411ee419692fe68f721247e0502b6c7382f
Mohammed Ahmed <mohammed_ahmed4949@yahoo.com>
2025-04-05 22:24:02 +0200

    components-deep-dive/feature: enhance css using host element

--- components-deep-dive/src/app/shared/control/control.component.ts
+++ components-deep-dive/src/app/shared/control/control.component.ts
@@ -8,6 +8,10 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   // We used it for styling the inputs and the textareas that are passed from outside
   encapsulation: ViewEncapsulation.None,
+  // This prop is to attach an attribute, event or anything to this component's host
+  host: {
+    class: 'control',
+  },
 })
 export class ControlComponent {
   label = input.required<string>();
