c2c121c6a625831e3b07bd9d9210ae3e5ca591fb
Mohammed Ahmed <mohammed_ahmed4949@yahoo.com>
2025-08-21 16:17:58 +0300

    feature: new ticket is added to support tickets on submit

--- components-deep-dive/src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
+++ components-deep-dive/src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  output,
+  viewChild,
+  ViewChild,
+} from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
-import { ControlComponent } from "../../../shared/control/control.component";
+import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  private form = viewChild<ElementRef<HTMLFormElement>>('form');
+  add = output<{ title: string; text: string }>();
+  onSubmit(titleValue: string, requestValue: string) {
+    this.add.emit({ title: titleValue, text: requestValue });
+    this.form()?.nativeElement.reset();
+  }
+}
--- components-deep-dive/src/app/dashboard/support-tickets/support-tickets.component.ts
+++ components-deep-dive/src/app/dashboard/support-tickets/support-tickets.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { NewTicketComponent } from "./new-ticket/new-ticket.component";
+import { NewTicketComponent } from './new-ticket/new-ticket.component';
+import { Ticket } from './support-tickets.model';
+import { TicketComponent } from "./ticket/ticket.component";
 
 @Component({
   selector: 'app-support-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
   templateUrl: './support-tickets.component.html',
-  styleUrl: './support-tickets.component.css'
+  styleUrl: './support-tickets.component.css',
 })
 export class SupportTicketsComponent {
+  tickets: Ticket[] = [];
 
+  onAdd({ title, text }: { title: string; text: string }) {
+    this.tickets.push({
+      id: Math.random().toString(),
+      title: title,
+      request: text,
+      status: 'opened',
+    });
+
+    console.log(this.tickets);
+  }
 }
